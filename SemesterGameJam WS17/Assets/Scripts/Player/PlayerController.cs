5a9d6e9040d107c65c89a7deb0bd8f26a1f89257
Marcel Bruckner <mbruckner94@gmail.com>
2017-12-08 23:36:26 +0100

    walking, lane switching

---
+++ SemesterGameJam WS17/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public int lane = 0;
+    private float lastLaneSwitch = 0;
+
+    public float gravityStrength = 9.81f;
+
+    private Vector3[] laneMiddles = new Vector3[] {
+        new Vector3(0,0,0),
+    };
+    private float laneWidth = 1.2f;
+
+    private Rigidbody rb;
+    private Vector3 gravity = Vector3.down;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(gravity * gravityStrength);
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3(lane * laneWidth, transform.position.y, transform.position.z), 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        GetInput();
+        //transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.forward * velocity, 0.5f);
+	}
+
+    void GetInput()
+    {
+        SwitchLane();
+    }
+
+    void SwitchLane()
+    {
+        int input = Input.GetButtonDown("Right") ? 1 : 0;
+        input = Input.GetButtonDown("Left") ? -1 : input;
+
+        if (Time.time - lastLaneSwitch > 0.1f && input != 0)
+        {
+            lane += input;
+            if (lane < -1 || lane > 1)
+            {
+                if (lane < -1)
+                    lane = -1;
+                if (lane > 1)
+                    lane = 1;
+                input = 0;
+            }
+
+            print("Lane: " + lane);
+            lastLaneSwitch = Time.time;
+            if(input != 0)
+                rb.AddForce(Vector3.up * 80f);
+        }
+    }
+}
